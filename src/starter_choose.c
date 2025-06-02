07661b464eef7367e7b5f3bd40ee2de0ff9be702
Curtis Henry <curtishenry117@gmail.com>
2025-06-02 07:31:44 -0600

    updated Espeon and Umbreon sprites and pallets

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -112,9 +112,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_TREECKO,
-    SPECIES_UNOWN,
-    SPECIES_MUDKIP,
+    SPECIES_SUNKERN,
+    SPECIES_SLUGMA,
+    SPECIES_REMORAID,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
